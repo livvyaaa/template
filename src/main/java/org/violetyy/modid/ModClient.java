d43826ebdc40e3062bb943773bf9befbdb514c48
violet <v.violette.yy@gmail.com>
2025-03-23 16:59:38 +0800

    init

---
+++ src/main/java/org/violetyy/modid/ModClient.java
@@ -0,0 +1,10 @@
+package org.violetyy.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	
+	}
+}
